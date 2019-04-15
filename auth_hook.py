ef4110807ed111008d35ed740778e08e3d31902c
Dick Snel <d.snel@dsolvit.com>
2019-04-15 13:38:12 +0200

    Skip auth if loginurl not provided

--- auth_hook.py
+++ auth_hook.py
@@ -11,7 +11,8 @@ def start_zap(port, extra_zap_params):
     webdriver.load_from_extra_zap_params(port, extra_zap_params)
 
 def zap_access_target(zap, target):
-    webdriver.setup_zap_context(zap, target)
-    webdriver.setup_webdriver(zap, target)
-    webdriver.login(zap, target)
-    webdriver.cleanup()
+    if webdriver.auth_loginUrl:
+        webdriver.setup_zap_context(zap, target)
+        webdriver.setup_webdriver(zap, target)
+        webdriver.login(zap, target)
+        webdriver.cleanup()
